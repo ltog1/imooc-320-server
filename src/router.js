e40b7caf96225957aaf1fca4a0986d17480974be
王福朋 <wangfupeng1988@163.com>
2022-08-07 18:03:32 +0800

    actions 发布测试

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ const ENV = process.env.NODE_ENV
 router.get('/api/test', async (ctx, next) => {
     ctx.body = {
         errno: 0,
-        msg: `koa2 test ok, ENV: ${ENV}`
+        msg: `koa2 test, ENV: ${ENV}`
     }
 })
 
